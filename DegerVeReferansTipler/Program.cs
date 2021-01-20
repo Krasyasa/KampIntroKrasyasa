7aedc7a5e81c3f92f7d86d53dfb125caf864e021
caganparmaksiz162005@gmail.com <caganparmaksiz162005@gmail.com>
2021-01-20 23:54:27 +0300

    20.01.2021 Kamp 4. Gün kodları

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //int,decimal,float,double,bool = değer tip
+            //class,interface,array = referans tip
+
+        }
+    }
+}
---
+++ UcNokta/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UcNokta
+{
+    class SepetManager
+    {
+        //naming convention
+        public void Ekle(Urun urun)//Class
+        {
+            Console.WriteLine("Sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi) //Class değil
+        {
+            Console.WriteLine("Sepete eklendi : " + urunAdi);
+        }
+    }
+        
+
+    
+}
+
